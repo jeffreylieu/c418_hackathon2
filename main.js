aa9d2a7586ce06fd6c685c540ee146f0c9b79b7c
dburns1 <patriciadburns@gmail.com>
2018-06-05 12:05:55 -0700

    twitter Ajax function

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-$(document).ready();
+$(document).ready(intializingApp);
+
+function intializingApp(){
+    console.log("hello"); 
+    twitterRequest(); 
+}
+
+var twitterArray=[]; 
+
+function twitterRequest (){
+    var twitterObject={
+          url: ' https://s-apis.learningfuze.com/hackathon/twitter/index.php',
+          method: 'get', 
+          dataType: 'json',
+          success:function(result){
+              console.log(result.tweets.statuses[i].text);
+              
+            //   var twitterArray=[];
+            //   twitterArray.push(result); 
+
+          },
+          data:{
+               "search_term":"werewolves",
+            //    "iso_language_code":"en",
+            //    "hastags": "#werewolves",         
+          },    
+          metadata:{
+              "iso_language_code":"en"
+          }
+    }
+    $.ajax(twitterObject); 
+}
