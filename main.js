11eea93d226ea530399df4a42ab0538660f37c2b
StephanieLDR <stephanie.c.lacina@gmail.com>
2018-06-05 12:33:12 -0700

    Added moonphase api instead of youtube

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 $(document).ready();
+
+function getMoonData(year = (new Date()).getFullYear()) {
+
+    var ajaxConfig = {
+        url: 'http://api.usno.navy.mil/moon/phase',
+        method: "GET",
+        dataType: 'JSON',
+        data: {
+            year
+        },
+
+        success: function (result) {
+            console.log('2) AJAX Success function called, with the following result:', result);
+
+        }
+    };
+    $.ajax(ajaxConfig)
+}
